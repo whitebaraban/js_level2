e86685952f7a65ae6315536c6246a856410c3114
Barabanov Pavel <barabanofwork@yandex.ru>
2019-04-04 23:44:45 +0300

    Накидал стили и создал массив JS

---
+++ script.js
@@ -0,0 +1,12 @@
+const GOODS = [
+	{title: 'Book', price: '230$'},
+	{title: 'T-short', price: '50$'},
+	{title: 'Milk', price: '0.99$'},
+	{title: 'Potato', price: '0.20$'},
+	{title: 'Duck', price: '1.99$'},
+	{title: 'Iphone', price: '200$'},
+	{title: 'Pen', price: '0.20$'},
+	{title: 'Chicken', price: '2$'}
+];
+
+console.log(GOODS);
